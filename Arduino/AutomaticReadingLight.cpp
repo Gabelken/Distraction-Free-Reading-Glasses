b79184df38f65c3946ae9f03d495bf6b05c9e22b
gkenna <gkenna899@gmail.com>
2020-04-14 13:26:40 -0700

    arduino - update documentation

--- Arduino/AutomaticReadingLight.cpp
+++ Arduino/AutomaticReadingLight.cpp
@@ -1,15 +1,29 @@
 #include <Arduino.h>
 
+/**
+ * Class to control light-sensitive reading lights situated on the glasses.
+ * 
+ * If the glasses are on, reads the light level and turns LEDs on if the area is too dim. Increases in brightness as the room gets dimmer.
+ * 
+ * Requres: 
+ * - Any number of LEDs connected in parallel to a PMW pin through a resistor
+ * - A photocell connected to an analog pin
+*/
 class AutomaticReadingLight {
   private:
+    // Pins used
     const byte ledPin;
     const byte sensorPin;
-    const int threshold = 100;
-    const int minBrightness = 80;
-    float brightnessScale;
+    
+    //Tuning values for the photocell
+    const int threshold = 100; // Sensor threshold at which the room is deemed 'too dim'
+    const int minBrightness = 80; //Lowest brightness setting of the LEDs
+    float brightnessScale; // Scale at which leds brighten or dim to adjust to the ambient light level. Set based on the tuned values.
     
   public:
+    // Constructor - Set pins and setup brightness scale value based on tuning values
     AutomaticReadingLight(byte ledPin, byte sensorPin) : ledPin(ledPin), sensorPin(sensorPin) {
+      //Brightness is adjusted to be between minBrightness and 255, based on distance from the threshold
       brightnessScale = (255-minBrightness);
       brightnessScale /= threshold;
     }
@@ -19,21 +33,28 @@ class AutomaticReadingLight {
       pinMode(sensorPin, INPUT);
     }
     
+    // Function to be called every time the program loops
     void loop(bool isOn) {
       if(isOn) {
+        // Check the ambient light level
         int lightLevel = analogRead(sensorPin);
+        
+        // If the light level is dimmer than the tuned threshold, turn the lights on
         if(lightLevel <= threshold) {
           
-          // Calc 
+          // Find how far the light level is from the threshold
           float brightnessDiff = threshold-lightLevel;
-
+          
+          // Increase brightness based on how much worse the reading is than the threshold.
           int scaledBrightnessDiff = (int)(brightnessDiff*brightnessScale);
           int brightness = scaledBrightnessDiff + minBrightness;
           
+          // Set the LED(s)
           analogWrite(ledPin, brightness);
           return;
         }
       }
+      // If the glasses are off, or the area is bright enough, turn off the LED(s)
       analogWrite(ledPin, 0);
     }
   
--- Arduino/Communication.cpp
+++ Arduino/Communication.cpp
@@ -1,35 +1,41 @@
 #include <Arduino.h>
 
+/**
+ * Class to send and receive signals to and from an Android app.
+ * 
+ * Checks for connections from a phone and sends updates as required
+ * 
+ * Requres: 
+ * - A bluetooth module
+*/
 class CommunicationHandler {
   private:
+    // Storage for incoming data
     const byte numChars = 10;
     char receivedChars[10];
+    byte ndx = 0; //Index of the next char in the buffer string
     
+    // Settings for parsing incoming data
     const char startMarker = '<';
     const char endMarker = '>';
     boolean recvInProgress = false;
-    byte ndx = 0; //Index of the next char in the buffer string
     
+    // Lets the controller know a signal is read in and ready to be handled
     boolean newData = false;
 
+    // Tracks whether there is a phone connected to the Arduino that needs to recieve updates
     bool isConnected = false;
     
-    int testLED_R = 46;
-    int testLED_St = 42;
-    int testLED_G = 38;
-    int testLED_En = 34;
-    
     /**
-      Source: https://forum.arduino.cc/index.php?topic=396450
-      Read a line of data in from the serial port, one character at a time. ***Non-blacking - MUST be called from loop***
+      Read a line of data in from the serial port, one character at a time. ***Non-blacking - MUST be called from loop or other non-blocking function***
       Only read data between start and end markers to avoid unwanted behaviour.
 
-      Results are stored in 'receivedChars'.
+      Results are stored in 'receivedChars'
     */
     void recvWithStartEndMarkers() {
       char rc; //Char read in
 
-      if (Serial.available() > 0){// && newData == false) {
+      if (Serial.available() > 0) {
         rc = Serial.read();
 
         // When recieving, accept any characters until the end marker
@@ -57,6 +63,8 @@ class CommunicationHandler {
 
     /**
       Parse data received over serial into something the program can use.
+      
+      Returns the receieved data as an integer.
     */
     int parseReadData() {
       int result = atoi(receivedChars);
@@ -67,28 +75,45 @@ class CommunicationHandler {
     void setup() {
       Serial.begin(9600);
     }
-
+    
+    /**
+     * Send an update to any connected device.
+     * If a phone is currently connected, send a message over serial telling it whether the glasses are on or off
+     * 
+     * isOn {bool} the state of the glasses
+     */
     void Send(bool isOn) {
       if (isConnected) {
-        Serial.print('<');
-        int msg = (isOn) ? 1 : 0;
+        Serial.print('<'); //Use brackets so the app can know when the whole message has arrived, and that it came from the right place
+        int msg = (isOn) ? 1 : 0; // Send 1 if on, or 0 if off
         Serial.print(msg);
         Serial.println('>');
       }
     }
     
+    /**
+     * Reads incoming messages from the serial port to see if a device has signalled that it's ready.
+     * If a new device has connected, send it the current state.
+     * 
+     * isOn {bool} the state of the glasses
+     * 
+     * Returns true if a phone is connected and listening, else false
+     */
     bool CheckForPhone(bool isOn) {
-      recvWithStartEndMarkers();
+      //Read incoming bits
+      recvWithStartEndMarkers(); 
+      
+      // Once a whole message has been recieved, handle it
       if(newData) {
-        int control = parseReadData();
-        if(control == 0) {
+        int control = parseReadData(); 
+        if(control == 0) { // Phone has disconnected and no longer expects data
           isConnected = false;
         }
-        else if(control == 1) {
+        else if(control == 1) { //Phone has connected and expects data. It needs to know the current state
           isConnected = true;
+          Send(isOn);
         }
-        Send(isOn);
-        newData = false;
+        newData = false; // Reset newData to start looking for a new message
       }
       return isConnected;
     }
